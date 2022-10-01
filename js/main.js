7a415e31c76125911b7785a1ab4ad77f5e196501
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2022-10-01 00:18:11 -0400

    Fixed more problems - h1 title next

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var link = document.getElementsByTagName('a'),
     tutVid = document.getElementById('tutVidTitle');
 
-for(var i=0; i<5; i++){
-  link[i].onclick = function(){
+for(var i=0; i < 9; i++ ){
+  
+    link[i].onclick = function(){
     tutVid.innerHTML = this.innerHTML;
   };
 }
