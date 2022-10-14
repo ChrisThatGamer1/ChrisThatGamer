d93d9178156e1fd1ab432b5cab065381a2692078
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2022-10-13 23:59:58 -0400

    Added Disclaimer
    Family gave advice, that there should not be bad language in these videos. So instead., I added a disclaimer that explains, these videos may have bad words in them, etc.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ for(var i=0; i < 9; i++ ){
     tutVid.innerHTML = this.innerHTML;
   };
 }
+
+  // show alert box, that these videos may contain some sort of adult content, i.e swearing, etc.
+  function showAlert() {
+    var myText = "WARNING: The videos below contain explicit content. Please be aware that you are about to view, content that some viewers may find offensive. Viewers discretion is advised.";
+    var myText2 = "The video(s) highlight ChrisThatGamer's competitive Call of Duty career. How he edited videos, recored videos, and created thumbnails/scripts.";
+    alert (myText + "\n\n" + myText2);
+  }
+  
